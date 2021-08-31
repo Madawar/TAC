46a2e418a659ac555f36d9ecc4738dbae5d9dae5
Dennis Wanyoike Kuria <dwanyoike@codedcell.com>
2021-08-31 14:12:15 +0300

    Change Login Flow

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/flight';
 
     /**
      * The controller namespace for the application.
