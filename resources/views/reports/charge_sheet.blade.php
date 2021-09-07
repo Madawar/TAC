3a52b5ea0a73ca01e7844a40c7f14ee93c78c79e
Dennis Wanyoike Kuria <dwanyoike@codedcell.com>
2021-09-07 10:01:08 +0300

    Update charge_sheet.blade.php

--- resources/views/reports/charge_sheet.blade.php
+++ resources/views/reports/charge_sheet.blade.php
@@ -2,6 +2,7 @@
 <?php
 use Illuminate\Support\Str;
 ?>
+
 <head>
     <link rel="stylesheet" href="{{ url('css/bundle.css') }}" type="text/css" media="all" />
     <link rel="stylesheet" href="{{ url('css/dashboard.css') }}" type="text/css" media="all" />
@@ -60,19 +61,21 @@ use Illuminate\Support\Str;
 
     <hr />
 
-    <h1 class="sheetno"><i>S/NO </i> : {{ $flight->serial }}</h1>
-    <br />
-    <br />
+    <h1 class="
+            sheetno"><i>S/NO </i> : {{ $flight->serial }}</h1>
+            <br />
+            <br />
 
-    <table class="table card-table table-vcenter text-nowrap table-bordered">
+            <table class="table card-table table-vcenter text-nowrap table-bordered">
 
-        <tbody>
-            <tr>
-                <td colspan="4" style="font-weight: bold; background: #f6fbff;">Flight Details</td>
-            </tr>
-            <tr>
-                <td>
-                    Flight Number : <u><b class="">{{ $flight->carrier->carrier_code }} {{ $flight->flightNo }}</b></u>
+                <tbody>
+                    <tr>
+                        <td colspan="4" style="font-weight: bold; background: #f6fbff;">Flight Details</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Flight Number : <u><b
+                                    class="">{{ $flight->carrier->carrier_code }} {{ $flight->flightNo }}</b></u>
 
                 </td>
                 <td>
@@ -99,126 +102,137 @@ use Illuminate\Support\Str;
 
                 </td>
                 <td>
-                    ATD : <u class="text-right"><b>{{ $flight->departure }}</b></u>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    Aircraft Registration: <u><b>{{ $flight->aircraftRegistration }}</b></u>
+                    ATD : <u class=" text-right"><b>{{ $flight->departure }}</b></u>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            Aircraft Registration: <u><b>{{ $flight->aircraftRegistration }}</b></u>
 
-                </td>
-                <td colspan="2">
-                    Parking Stand ARR : <u><b></b></u>
-                </td>
+                        </td>
+                        <td colspan="2">
+                            Parking Stand ARR : <u><b></b></u>
+                        </td>
 
-            </tr>
-        </tbody>
+                    </tr>
+                </tbody>
 
-    </table>
-    <br />
-    <table class="table card-table table-vcenter text-nowrap table-bordered">
+            </table>
+            <br />
+            <table class="table card-table table-vcenter text-nowrap table-bordered">
 
-        <tbody>
-            <tr>
-                <td style="font-weight: bold; background: #f6fbff;">Services Provided</td>
-            </tr>
-            <tr style="height: 300px;">
-                <td class=""
-                    style="width: 300px; height: 80px; vertical-align: top; text-align: justify;font-weight: bold;">
-                    1.) {{ $flight->turnaround_type }}
+                <tbody>
+                    <tr>
+                        <td style="font-weight: bold; background: #f6fbff;">Services Provided</td>
+                    </tr>
+                    <tr style="height: 300px;">
+                        <td class=""
+                    style=" width: 300px; height: 80px; vertical-align: top;
+                            text-align: justify;font-weight: bold;">
+                            1.) {{ $flight->turnaround_type }}
 
-                </td>
-            </tr>
-            <tr>
-                <td style="font-weight: bold; background: #f6fbff;">Incidental Services Provided</td>
-            </tr>
-            <tr>
-                <td class=""
-                    style="width: 300px; height: 80px; vertical-align: top; text-align: justify;font-weight: bold;">
-                    @foreach ($flight->services as $incidental)
-                        <b>{{ $loop->index + 1 }}.)</b> {{ $incidental->service }}
-                        @if ($incidental->start_time != '' || $incidental->start_time != null)
-                            - {{ $incidental->start_time }} {{ $incidental->end_time }} ,{{ $incidental->remarks }}
-                        @else
-                            - {{ $incidental->qty }}
-                            @if ((int) $incidental->qty > 0)
-                                ,{{ $incidental->remarks }}
-                            @else
-                                ,{{ $incidental->remarks }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight: bold; background: #f6fbff;">Incidental Services Provided</td>
+                    </tr>
+                    <tr>
+                        <td class=""
+                    style=" width: 300px; height: 80px; vertical-align: top;
+                            text-align: justify;font-weight: bold;">
+                            @foreach ($flight->services as $incidental)
+                                <b>{{ $loop->index + 1 }}.)</b> {{ $incidental->service }}
+                                @if ($incidental->start_time != '' || $incidental->start_time != null)
+                                    - {{ $incidental->start_time }} {{ $incidental->end_time }}
+                                    ,{{ $incidental->remarks }}
+                                @else
+                                    - {{ $incidental->qty }}
+                                    @if ((int) $incidental->qty > 0)
+                                        ,{{ $incidental->remarks }}
+                                    @else
+                                        ,{{ $incidental->remarks }}
+                                    @endif
+
+                                @endif
+                                <br />
+                            @endforeach
+
+                        </td>
+
+
+                    </tr>
+                </tbody>
+
+            </table>
+            <br />
+
+            <table class="table card-table table-vcenter text-nowrap table-bordered">
+
+                <tbody>
+                    <tr>
+                        <td colspan="2" style="font-weight: bold; background: #f6fbff;">Airline</td>
+                    </tr>
+                    <tr style="height: 300px;">
+                        <td
+                            style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
+                            Airline Representative<br />
+                            {{ $flight->signature_name }}
+
+                        </td>
+                        <td
+                            style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
+                            Signature<br />
+                            @if ($flight->signature != null)
+                                <img src="{{ url('/storage/signatures/' . $flight->signature) }}"
+                                    style="max-width:auto; height:auto;" />
                             @endif
+                        </td>
 
-                        @endif
-                        <br />
-                    @endforeach
+                    </tr>
 
-                </td>
+                    <tr>
+                        <td colspan="2" style="font-weight: bold; background: #f6fbff;">GHA</td>
+                    </tr>
+                    <tr>
+                        <td
+                            style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
+                            AFS Representative<br />
+                            @if ($user)
+                                {{ $user->name }}
+                            @endif
+                        </td>
+                        <td
+                            style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
+                            Signature<br />
+                            @if ($user)
+                                <img src="{{ url('/storage/signatures/' . $user->signature) }}"
+                                    style="max-width:auto; height:auto;" />
+                            @endif
+                        </td>
 
+                    </tr>
 
-            </tr>
-        </tbody>
+                </tbody>
+            </table>
+            <br />
+            <table class="table card-table table-vcenter text-nowrap table-bordered">
 
-    </table>
-    <br />
+                <tbody>
+                    <tr>
+                        <td style="font-weight: bold; background: #f6fbff;">Comments</td>
+                    </tr>
+                    <tr style="height: 300px;">
+                        <td
+                            style="width: 300px; height: 60px; vertical-align: top; text-align: left;font-weight: bold;">
+                            {{ $flight->remarks }}
 
-    <table class="table card-table table-vcenter text-nowrap table-bordered">
+                        </td>
 
-        <tbody>
-            <tr>
-                <td colspan="2" style="font-weight: bold; background: #f6fbff;">Airline</td>
-            </tr>
-            <tr style="height: 300px;">
-                <td style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
-                    Airline Representative<br />
-                    {{ $flight->signature_name }}
+                    </tr>
 
-                </td>
-                <td style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
-                    Signature<br />
-                    @if($flight->signature != null)
-                        <img src="{{ url('/storage/signatures/' . $flight->signature) }}" style="max-width:auto; height:auto;" />
-                    @endif
-                </td>
-
-            </tr>
-
-            <tr>
-                <td colspan="2" style="font-weight: bold; background: #f6fbff;">GHA</td>
-            </tr>
-            <tr>
-                <td style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
-                    AFS Representative<br/>
-                    {{ $user->name }}
-
-                </td>
-                <td style="width: 300px; height: 90px; vertical-align: top; text-align: left;font-weight: bold;">
-                    Signature<br/>
-                    @if($user->signature != null)
-                     <img src="{{ url('/storage/signatures/' . $user->signature) }}" style="max-width:auto; height:auto;" />
-                    @endif
-                </td>
-
-            </tr>
-
-        </tbody>
-    </table>
-    <br />
-    <table class="table card-table table-vcenter text-nowrap table-bordered">
-
-        <tbody>
-            <tr>
-                <td style="font-weight: bold; background: #f6fbff;">Comments</td>
-            </tr>
-            <tr style="height: 300px;">
-                <td style="width: 300px; height: 60px; vertical-align: top; text-align: left;font-weight: bold;">
-                    {{ $flight->remarks }}
-
-                </td>
-
-            </tr>
-
-            </tr>
-        </tbody>
-    </table>
+                    </tr>
+                </tbody>
+            </table>
 
 </body>
 
